cc27000c154eb7ea7ee137398f5bead0c0574b2b
Nick S <nsardonini@gmail.com>
2021-05-25 17:10:14 -0400

    add findNode, findParent, and remove functions to bst

--- bst.js
+++ bst.js
@@ -33,6 +33,87 @@ BST.prototype.add = function (val, curr = this.root) {
   return this;
 };
 
+BST.prototype.remove = function (val, curr = this.root) {
+  if (!curr || !this.search(val)) return false;
+  if (!curr.left && !curr.right) return false;
+
+  const parent = this.findParent(val);
+  const node = this.findNode(val);
+
+  if (!node.left && !node.right) {
+    // No children
+    if (node.val < parent.val) {
+      parent.left = node.right;
+    } else {
+      parent.right = node.left;
+    }
+  } else if (node.left && node.right) {
+    // Two children
+    let next = node.right;
+    while (next.left) {
+      next = next.left;
+    }
+    if (next !== node.right) {
+      this.remove(next.val);
+      node.val = next.val;
+    } else {
+      node.val = next.val;
+      node.right = node.right.right;
+    }
+  } else {
+    let next;
+    if (!node.left) {
+      next = node.right;
+    } else {
+      next = node.left;
+    }
+    if (curr === node) {
+      this.root = next;
+    } else if (parent.left === node) {
+      parent.left = next;
+    } else if (parent.right === node) {
+      parent.right = next;
+    }
+  }
+};
+
+BST.prototype.findNode = function (val, node = this.root) {
+  if (!node || !this.search(val)) return false;
+  let curr = node;
+
+  while (curr.val !== val) {
+    if (val < curr.val) {
+      curr = curr.left;
+    } else {
+      curr = curr.right;
+    }
+  }
+
+  return curr;
+};
+
+BST.prototype.findParent = function (val, node = this.root) {
+  if (!node || !this.search(val)) return false;
+
+  if (val < node.val) {
+    if (!node.left) {
+      return false;
+    } else if (node.left.val === val) {
+      return node;
+    } else {
+      return this.findParent(val, node.left);
+    }
+  } else {
+    if (!node.right) {
+      return false;
+    } else if (node.right.val === val) {
+      return node;
+    } else {
+      return this.findNode(val, node.right);
+    }
+  }
+};
+
 BST.prototype.search = function (val, curr = this.root) {
   let contains = false;
 
